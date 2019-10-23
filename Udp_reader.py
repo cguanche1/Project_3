222256f4ce1c8f8d17053ca71f5962a8ad911a3a
cguanche1 <carlos.guanche@yale.edu>
2019-10-23 03:29:56 -0400

    Re-doing the udp reader

--- Udp_reader.py
+++ Udp_reader.py
@@ -1,8 +1,8 @@
 
 import socket
 
-UDP_IP = ""
-UDP_PORT = 
+UDP_IP = "198.168.1.2"
+UDP_PORT = 57222
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -10,4 +10,4 @@ sock.bind((UDP_IP,UDP_PORT))
 
 while True:
 	data, addr = sock.recvfrom(1024)
-	print "received message: ", data
+	print ("received message: {}", .fortmat(data))
